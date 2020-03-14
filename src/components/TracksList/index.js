b742628a7a2845761d197c8d10d6723d39ce78fc
coreyarch1234 <harrilal@mit.edu>
2020-03-13 23:47:57 -0700

    successfully gets artist info

--- src/components/TracksList/index.js
+++ src/components/TracksList/index.js
@@ -1,18 +1,23 @@
 import React from "react";
 import { observable, action } from "mobx";
 import { observer } from "mobx-react";
+import { getSongsByArtist } from "../../services/TransportLayer";
 
 @observer
 class TracksList extends React.Component {
   @observable tracks = [];
 
-  componentDidMount() {
+  async componentDidMount() {
     const accessToken = localStorage.getItem("access_token");
+
+    this.setTracks();
   }
 
   @action
-  setTracks = () => {
+  setTracks = async () => {
   // make API call
+  const res = await getSongsByArtist("tania%20bowra");
+  console.log(res.data)
   }
 
   render() {
--- src/services/TransportLayer.js
+++ src/services/TransportLayer.js
@@ -3,6 +3,12 @@ import axios from "axios";
 const loginString = `https://accounts.spotify.com/authorize?client_id=9096ba7eb7994e908c31dae5d5d91a3c&redirect_uri=http://localhost:3000&scope=user-read-private%20user-read-email&response_type=token&state=123&show_dialog=true`
 const baseURL = "https://api.spotify.com/v1/search";
 
+const accessToken = localStorage.getItem("access_token");
+
 export const login = () => axios.get(loginString);
 
-export const getSongsByArtist = (artist) => axios.get(`${baseURL}?q=${artist}`);
+export const getSongsByArtist = (artist) => axios.get(`${baseURL}?q=${artist}&type=artist`, {
+  headers: {
+    Authorization: "Bearer " + accessToken //the token is a variable which holds the token
+  }
+});
