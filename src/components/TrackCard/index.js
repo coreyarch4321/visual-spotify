a5ab91791ea2be019a6bfd5c034158a575ae52e9
coreyarch1234 <harrilal@mit.edu>
2020-03-15 03:33:50 -0700

    solid game logic

--- src/components/TrackCard/index.js
+++ src/components/TrackCard/index.js
@@ -5,23 +5,18 @@ import CardActionArea from '@material-ui/core/CardActionArea';
 import CardContent from '@material-ui/core/CardContent';
 import CardMedia from '@material-ui/core/CardMedia';
 import Typography from '@material-ui/core/Typography';
+import Skeleton from '@material-ui/lab/Skeleton';
 
-const TrackCard = observer(({ title, url }) => {
-  console.log({ title, url })
+const TrackCard = observer(({ title, url, onClick }) => {
   return (
-    <Card>
+    <Card onClick={onClick}>
       <CardActionArea>
         <CardMedia
-          title={title}
           component={() => <img src={url} />}
         />
         <CardContent>
           <Typography gutterBottom variant="h5" component="h2">
-            Lizard
-          </Typography>
-          <Typography variant="body2" color="textSecondary" component="p">
-            Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging
-            across all continents except Antarctica
+            {title}
           </Typography>
         </CardContent>
       </CardActionArea>
--- src/components/TrackComparison/index.js
+++ src/components/TrackComparison/index.js
@@ -6,11 +6,21 @@ import { getTopTracks } from "../../services/TransportLayer";
 import CircularProgress from '@material-ui/core/CircularProgress';
 import TrackCard from "../TrackCard";
 
+// comparison answer values
+const LEFT_TRACK = "left_track";
+const RIGHT_TRACK = "right_track";
+
+// round outcome values
+const TIE = "tie";
+const WON = "won";
+const LOST = "lost";
+
 @observer
 class TrackComparison extends React.Component {
   @observable tracks = [];
   @observable leftTrack = {}
   @observable rightTrack = {};
+  @observable roundOutcome = null;
 
   @observable isLoading = false;
 
@@ -28,10 +38,14 @@ class TrackComparison extends React.Component {
       const tracks = response.data.items;
 
       runInAction(() => {
+        // set user's top tracks
         this.tracks = Array.from(tracks);
+
+        // set left and right tracks to compare
+        this.leftTrack = this.randomTrack();
+        this.rightTrack = this.randomTrack();
       });
 
-      this.setTracksToCompare();
     } catch (err) {
       console.error(`Failed to initialize: ${err}`);
     } finally {
@@ -39,13 +53,43 @@ class TrackComparison extends React.Component {
     }
   }
 
+  // compare tracks and reset tracks
   @action
-  setTracksToCompare = () => {
-    this.leftTrack = this.randomTrack();
-    this.rightTrack = this.randomTrack();
+  handleCompareTracks = (key) => {
+    console.log(this.leftTrack.popularity)
+    let winningTrack = null;
+
+    // set the winning track depending on which track had higher popularity
+    if (this.leftTrack.popularity > this.rightTrack.popularity) {
+      winningTrack = LEFT_TRACK;
+    } else if (this.leftTrack.popularity < this.rightTrack.popularity) {
+      winningTrack = RIGHT_TRACK
+    } else {
+      winningTrack = TIE;
+    }
+
+    // check to see if user won, lost or tied by comparing key value to winning track
+    if (winningTrack === TIE) {
+      this.roundOutcome = TIE;
+    } else {
+      if (key === winningTrack) {
+        this.roundOutcome = WON;
+      } else {
+        this.roundOutcome = LOST;
+      }
+    }
+
+    setTimeout(this.setNewRound, 2000)
   }
 
-  // gets random tracks to compare
+  @action
+  setNewRound = () => {
+    this.leftTrack = this.randomTrack();
+    this.rightTrack = this.randomTrack();
+
+    this.roundOutcome = null;
+  }
+
   randomTrack = () => {
     return this.tracks[Math.floor(Math.random() * this.tracks.length)]
   }
@@ -54,19 +98,32 @@ class TrackComparison extends React.Component {
     if (this.isLoading) {
       return <CircularProgress color="secondary" />
     } else {
-      console.log("this.leftTrack: ",this.leftTrack)
+      console.log("this.roundOutcome: ",this.roundOutcome)
       return (
         <Container>
           <LeftTrack>
             {this.leftTrack && (
               <TrackCard
-                title={this.leftTrack.name}
-                url={this.leftTrack.album && this.leftTrack.album.images && this.leftTrack.album.images[0] && this.leftTrack.album.images[0].url}
+                title={this.leftTrack && this.leftTrack.name}
+                url={this.leftTrack && this.leftTrack.album && this.leftTrack.album.images && this.leftTrack.album.images[0] && this.leftTrack.album.images[0].url}
+                onClick={() => this.handleCompareTracks(LEFT_TRACK)}
               />
             )}
           </LeftTrack>
+          <ResultContainer>
+            {this.roundOutcome === null && <ResultText>OR</ResultText>}
+            {this.roundOutcome === WON && <ResultText>WON</ResultText>}
+            {this.roundOutcome === LOST && <ResultText>LOST</ResultText>}
+            {this.roundOutcome === TIE && <ResultText>TIE</ResultText>}
+          </ResultContainer>
           <RightTrack>
-            <SongContainer>{this.rightTrack && this.rightTrack.name}</SongContainer>
+            {this.rightTrack && (
+              <TrackCard
+                title={this.rightTrack.name}
+                url={this.rightTrack.album && this.rightTrack.album.images && this.rightTrack.album.images[0] && this.rightTrack.album.images[0].url}
+                onClick={() => this.handleCompareTracks(RIGHT_TRACK)}
+              />
+            )}
           </RightTrack>
         </Container>
       )
@@ -79,18 +136,33 @@ const Container = styled.div`
   flex-direction: row;
   justify-content: center;
   align-items: center;
+  margin-top: 40px;
 `;
 
 const LeftTrack = styled.div`
-
+  padding: 16px;
 `;
 
 const RightTrack = styled.div`
-
+  padding: 16px;
 `;
 
-const SongContainer = styled.div`
-  font-size: 16px;
+const ResultContainer = styled.div`
+  text-align: center;
+  background: #333;
+  border-radius: 100%;
+`;
+
+const ResultText = styled.div`
+  width: 100px;
+  height: 100px;
+  margin: 0 auto;
+  padding: 0;
+  display: inline-block;
+  line-height: 100px;
+  text-align: center;
+  font-size: 32px;
+  color: white;
 `;
 
 export default TrackComparison;
